4b0e1b0ad24f235586b2903cb584d87432ae9ab9
John De La Rosa <john.delarosa@ideapublicschools.org>
2020-03-02 20:34:00 -0600

    change icon picture and add card and table

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
+  <div class="bg-red-600 min-h-screen">
+    <NavBar />
     <router-view />
   </div>
 </template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+  },
+}
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="flex items-center justify-between flex-wrap bg-red-300 p-6">
+    <router-link to="/" class="flex items-center flex-shrink-0 text-white mr-6">
+      <img class="h-8 mr-4" src="superlogo.png" alt="Logo" />
+      <span class="font-semibold text-xl tracking-tight"
+        >SuperHero<span class="font-light">Finder</span></span
+      >
+    </router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+}
+</script>
